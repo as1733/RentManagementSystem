9e320cc72e77aab46d6f565d61e7dd7f8f0b9277
Aryan Singh <ary1733@gmail.com>
2021-10-08 09:33:00 +0530

    About us Conflict resolved

--- rms/src/App.js
+++ rms/src/App.js
@@ -1,14 +1,22 @@
-import "./App.css";
-import NavigationBar from "./components/MenuComponent";
-import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import NavigationBar from './components/MenuComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle";
 import Carousel from "./components/carousel";
-
+import About from './pages/About';
+import {Switch,Route} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <NavigationBar />
-      <Carousel></Carousel>
+    <div className='App'>
+      <Switch>
+        <Route path='/' exact>
+          <NavigationBar />
+          <Carousel></Carousel>
+        </Route>
+        <Route path="/aboutus">
+          <About />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- rms/src/components/MenuComponent.js
+++ rms/src/components/MenuComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import styled from "styled-components";
 import ReactBootstrap, {
   Jumbotron,
@@ -61,6 +62,7 @@ class NavigationBar extends Component{
                         <Nav className="me-auto">
                             <Nav.Link href="#features">Features</Nav.Link>
                             <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <Nav.Link href='aboutus'>About Us</Nav.Link>
                             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -90,6 +92,7 @@ class NavigationBar extends Component{
                                     this.state.isTokenDetected ? 'Login' : 'Sign Up'
                                 }
                             </Button>
+                            <Link to='/aboutus' className='nav-link'>About Us </Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -102,56 +105,6 @@ class NavigationBar extends Component{
         )
     }
 
-  render() {
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Rent Management Systems</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#About">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-            <Nav>
-            <Button variant="dark" onClick={() => this.handleModal(true)}>
-                { 
-                    this.state.isTokenDetected ? 'Login' : 'Sign Up'
-                }
-            </Button>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-        <UserAuthModal
-                    handleModal={this.handleModal}
-                    showModal={this.state.showModal}
-                    isTokenDetected={this.state.isTokenDetected}
-        />
-      </Navbar>
-    );
-  }
 }
 
 export default NavigationBar;
