146a38f3863836e22a2c95c6f4f71eb326ef0d5a
as1733 <as1733@gmail.com>
2021-10-09 09:29:52 +0530

    Added Backend server for the calling of api

--- backendapiserver/app.js
+++ backendapiserver/app.js
@@ -4,11 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var propertyApi = require('./routes/propertyApi')
 
-var app = express();
+var cors = require('cors')
+var app = express()
 
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,9 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../rms/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1/propertyapi',propertyApi)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
