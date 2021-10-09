b71deae4e6bcfa2741ac6dd8df053d38b9aac991
NirajGohel <nirajgohel.ng@gmail.com>
2021-10-09 13:13:50 +0530

    fix: render() and some styling

--- rms/src/components/MenuComponent.js
+++ rms/src/components/MenuComponent.js
@@ -80,18 +80,18 @@ class NavigationBar extends Component {
                             <FormControl
                                 type="search"
                                 placeholder="Search"
-                                className="mr-2"
+                                className="me-2"
                                 aria-label="Search"
                             />
-                            <Button variant="outline-success">Search</Button>
+                            <Button variant="outline-success" className="me-2">Search</Button>
                         </Form>
                         <Nav>
-                            <Button variant="dark" onClick={() => this.handleModal(true)}>
+                            <Button variant="secondary" onClick={() => this.handleModal(true)}>
                                 { 
                                     this.state.isTokenDetected ? 'Login' : 'Sign Up'
                                 }
                             </Button>
-                            <Link to='/aboutus' className='nav-link'>About Us </Link>
+                            <NavLink to='/aboutus' className='nav-link' activeClassName="active">About Us </NavLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -104,72 +104,6 @@ class NavigationBar extends Component {
         )
     }
 
-  render() {
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Rent Management Systems</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success" className="me-2">
-                Search
-              </Button>
-            </Form>
-            <Nav>
-              <Button
-                variant="secondary"
-                onClick={() => this.handleModal(true)}
-              >
-                {this.state.isTokenDetected ? "Login" : "Sign Up"}
-              </Button>
-              <NavLink
-                to="/aboutus"
-                className="nav-link"
-                activeClassName="active"
-              >
-                About Us{" "}
-              </NavLink>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-        <UserAuthModal
-          handleModal={this.handleModal}
-          showModal={this.state.showModal}
-          isTokenDetected={this.state.isTokenDetected}
-        />
-      </Navbar>
-    );
-  }
 }
 
 export default NavigationBar;
