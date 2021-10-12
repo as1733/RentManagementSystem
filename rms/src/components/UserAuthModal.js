bfc4541542fe5e710665f234c6f14307267d569b
Aryan Singh <ary1733@gmail.com>
2021-10-12 11:10:39 +0530

    Linking Login Auth

--- rms/src/components/UserAuthModal.js
+++ rms/src/components/UserAuthModal.js
@@ -1,44 +1,74 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { Modal, Form, Button } from 'react-bootstrap';
 
-const UserAuthModal = (props) => {
-  const [modalTitle, setModalTitle] = useState(false);
 
-  useEffect(() => {
-    setModalTitle(props.isTokenDetected);
-  }, [props.isTokenDetected])
 
-  return (
-    <>
-      <Modal
-        size="sm"
-        show={props.showModal}
-        onHide={() => props.handleModal(false)}
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>
-            {
-              modalTitle ? 'User Login' : 'User Sign Up'
-            }
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="my-4" controlId="email">
-              <Form.Label htmlFor="email">Email</Form.Label>
-              <Form.Control type="email" placeholder="Please enter email" />
-            </Form.Group>
-            <Form.Group className="my-4" controlId="password">
-              <Form.Label htmlFor="password">Password</Form.Label>
-              <Form.Control type="password" placeholder="Please enter password" />
-            </Form.Group>
-            <Button className="mt-2" type="submit">Submit</Button>
-          </Form>
-        </Modal.Body>
-      </Modal>
-    </>
-  )
+class UserAuthModal extends Component {
+  constructor(props){
+    super(props);
+    this.props = props;
+    this.state = {
+      email: "",
+      password: "",
+    }
+    this.handleLogin = this.handleLogin.bind(this);
 }
+  handleLogin(event){
+    event.preventDefault();
+    
+    fetch('/api/v1/auth/login', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username: this.state.email, password: this.state.password})
+  }).then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error=>{
+    console.log("Backend Server is not responding")
+    console.log(error)
 
+  })
+  .finally((e)=>{
+
+
+  }) ;
+  this.setState({email:"", password:""});
+  }
+
+
+  render(){
+    return (
+      <>
+        <Modal
+          size="sm"
+          show={this.props.showModal}
+          onHide={() => this.props.handleModal(false)}
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>
+              User Login 
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form  onSubmit={this.handleLogin}>
+              <Form.Group className="my-4" controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" value={this.state.email} onChange={e => this.setState({email: e.target.value})} placeholder="Please enter email" />
+              </Form.Group>
+              <Form.Group className="my-4" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" value={this.state.password} onChange={e => this.setState({password: e.target.value})} placeholder="Please enter password" />
+              </Form.Group>
+              <Button className="mt-2" type="submit">Submit</Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </>
+    )
+  }
+
+}
 export default UserAuthModal;
