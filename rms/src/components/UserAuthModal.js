0a216636ad2f1b8fe726a956adba3a3029207b33
Aryan Singh <ary1733@gmail.com>
2021-10-15 19:08:58 +0530

    JWT token integerated

--- rms/src/components/UserAuthModal.js
+++ rms/src/components/UserAuthModal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Modal, Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -10,9 +11,25 @@ class UserAuthModal extends Component {
     this.state = {
       email: "",
       password: "",
+      isLoggedIn:props.islogged
     }
     this.handleLogin = this.handleLogin.bind(this);
 }
+  handlelogout(event){
+    fetch('/api/v1/auth/logout', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+    }).then(response => response.json())
+    .then(data=>{
+      this.setState({isLoggedIn:'false'})
+    })
+
+  }
+
   handleLogin(event){
     event.preventDefault();
     
@@ -22,9 +39,13 @@ class UserAuthModal extends Component {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({username: this.state.email, password: this.state.password})
+    body: JSON.stringify({username: this.state.email, password: this.state.password}),
+    credentials: 'include',
   }).then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => console.log(data)
+  
+  
+  )
   .catch(error=>{
     console.log("Backend Server is not responding")
     console.log(error)
@@ -39,8 +60,35 @@ class UserAuthModal extends Component {
 
 
   render(){
+    const loggedIn=this.state.isLoggedIn
+    let modalTitle="Login"
+    let modalBody=''
+    if (loggedIn)
+    {
+      modalTitle="Logout"
+    modalBody= ( <a  className='nav-link' href="/api/v1/auth/logout">
+    
+    Are you Sure to logout?
+      </a>)
+      
+    }
+    else {
+
+
+      modalBody=           ( <Form  onSubmit={this.handleLogin}>
+      <Form.Group className="my-4" controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" value={this.state.email} onChange={e => this.setState({email: e.target.value})} placeholder="Please enter email" />
+      </Form.Group>
+      <Form.Group className="my-4" controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" value={this.state.password} onChange={e => this.setState({password: e.target.value})} placeholder="Please enter password" />
+      </Form.Group>
+      <Button className="mt-2" type="submit">Submit</Button>
+    </Form>)
+    }
     return (
-      <>
+      
         <Modal
           size="sm"
           show={this.props.showModal}
@@ -49,24 +97,14 @@ class UserAuthModal extends Component {
         >
           <Modal.Header closeButton>
             <Modal.Title>
-              User Login 
+              {modalTitle}
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Form  onSubmit={this.handleLogin}>
-              <Form.Group className="my-4" controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" value={this.state.email} onChange={e => this.setState({email: e.target.value})} placeholder="Please enter email" />
-              </Form.Group>
-              <Form.Group className="my-4" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" value={this.state.password} onChange={e => this.setState({password: e.target.value})} placeholder="Please enter password" />
-              </Form.Group>
-              <Button className="mt-2" type="submit">Submit</Button>
-            </Form>
+          {modalBody}
           </Modal.Body>
         </Modal>
-      </>
+   
     )
   }
 
